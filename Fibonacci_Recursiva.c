9dd799a538b00b9794f9425db19a33e1daeea349
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-01-11 21:00:02 -0300

    Coding.C

---
+++ Fibonacci_Recursiva.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int fibonacci(int position)
+{
+	if (position == 1 || position == 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return fibonacci(position - 1) + fibonacci(position - 2);
+	}
+}
+
+int main()
+{
+
+	int posicao, resultado;
+	printf("\n Escolha uma posiçao da sequencia de Fibonacci: ");
+	scanf("%d", &posicao);
+
+	if (posicao > 0)
+	{
+		resultado = fibonacci(posicao);
+		printf("\n\t Resultado: %d", resultado);
+	}
+	else
+	{
+		return printf("\n Esta posiçao nao existe na sequencia de Fibonacci");
+	}
+	return 0;
+}

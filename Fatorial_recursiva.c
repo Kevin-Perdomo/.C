6d5ad8b60bd9a994fe464fdf0ce195de90b215dc
Kevin-Perdomo <119632064+Kevin-Perdomo@users.noreply.github.com>
2023-01-14 12:50:47 -0300

    Fatorial_recursion!

--- Fatorial_recursiva.c
+++ Fatorial_recursiva.c
@@ -13,13 +13,18 @@ int main()
 {
 	int numero;
 	long int resultado;
-	printf("\n\t Digite o numero que deseja calcular e comfirme com Enter : ");
+
+	printf("\n\t Digite um numero natural positivo e comfirme com Enter : ");
 	scanf("%d",&numero);
-	if (numero >= 0){
-			resultado = fatorial(numero);
-			printf("\n\t %ld \n\n", resultado);
-	} else {
-		printf("\n\t Nao existe fatorial de numero negativo \n\n");
-	}
+
+	if (numero >= 0)
+	{	
+		resultado = fatorial(numero);
+		printf("\n\t %ld \n\n", resultado);
+	} else
+	     {
+		 	printf("\n\t Nao existe fatorial de numero negativo \n\n");
+		 }
+
     return 0;
 }
